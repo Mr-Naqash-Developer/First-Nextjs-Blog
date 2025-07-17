fb38c2eaca09780c39ef943cf64e1e7696cdcb9d
Mr-Naqash-Developer <mr.naqash.developer@gmail.com>
2025-07-17 16:47:32 +0500

    Corrected

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,8 @@ export default function NotFound() {
   return (
     <main className="text-center mt-20">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="mb-6">Oops! The page you're looking for doesn't exist.</p>
+      <p className="mb-6">Oops! The page you&#39;re looking for doesn&#39;t exist.</p>
       <Link href="/" className="text-blue-600 hover:underline">Go back home</Link>
     </main>
-  )
+  );
 }
